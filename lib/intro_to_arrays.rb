f6152925d7bfe8d36043c1ac883f6d79442652e9
Elyse Burmester <burmesterelyse@gmail.com>
2018-10-15 18:03:48 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,5 +6,17 @@ def array_with_two_elements
   array = ["Luna", "Jinx"]
 end
 
-def first_element
-  array
+def first_element(array)
+  array[0]
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array[-1]
+end
+
+def first_element_with_array_methods(array)
+  
